865e498635efc5eb243d1c290db9be2aa50fbd76
undertuga <undertuga@tutanota.de>
2015-08-13 18:16:27 +0100

    nyxeye.vm demo code fixs

--- core/req.core.js
+++ core/req.core.js
@@ -14,8 +14,7 @@ var errorlog = require('./logs.core.js').ErrorLog,
 
 
 // external libs
-var url = require('url'), 
-	fs = require('graceful-fs'),
+var url = require('url'),
 	mongo = require('./db.core.js'), 
 	conf = require('./cnf.core.js').syspaths;
 
@@ -47,14 +46,6 @@ ReqCore.prototype.parseRequest = function(req, callback){
 		};
 		
 		
-		
-		// logging prx request
-		//fs.appendFileSync(conf.traffic + ipv4, reqdata);
-		
-		
-		
-		
-		
 		// adding data to db
 		mongo.traffic.insert({
 			
@@ -71,9 +62,6 @@ ReqCore.prototype.parseRequest = function(req, callback){
 			if(!res){callback(null, false);}else{callback(null, true);}
 		
 		});
-		
-		
-		 
 	}
 };
 
